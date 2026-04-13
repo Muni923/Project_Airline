c53c6d378b22cef7ae5079fb6080650a56df9d32
Muni Chaurasiya <199281592+Muni923@users.noreply.github.com>
2026-04-13 11:41:45 +0530

    Added getAll cities API

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -59,5 +59,26 @@ async updateCity(cityId, data) { // {name: "Prayagraj"}
             throw {error};
         }
     }
+
+    async getAllCities(filter) { // filter can be empty also
+        try {
+            if(filter.name) {
+                const cities = await City.findAll({
+                    where: {
+                        name: {
+                            [Op.startsWith]: filter.name
+                        }
+                    }
+                });
+                return cities;
+            }
+            const cities = await City.findAll();
+            return cities;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+
 }
 module.exports = CityRepository;
