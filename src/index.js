fa5197d6dfb41a78e06a41bf5c0224abbde35168
Muni Chaurasiya <199281592+Muni923@users.noreply.github.com>
2026-05-24 00:38:40 +0530

    Added Airplanes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const { PORT } = require("./config/serverConfig");
 const ApiRoutes = require("./routes/index");
 
 const db = require("./models/index");
-// const { City, Airport } = require("./models/index");
+const { City, Airport,Airplane} = require("./models/index");
 
 const setupAndStartServer = async () => {
   // create the express object
@@ -23,7 +23,31 @@ const setupAndStartServer = async () => {
     if (process.env.SYNC_DB) {
       db.sequelize.sync({ alter: true });
     }
-  });
+  });   
 };
  
 setupAndStartServer();   
+
+// db.City.bulkCreate([{ name: 'mp1' }, { name: 'u1p' }]);//array of object
+// const city = db.City.findByPk(1, {
+//    include: Airport
+// });
+
+// console.log(city);
+
+
+// async function test(){
+
+//    const city = await City.findOne({
+//       where: {
+//          name: "cawnpore"
+//       },
+
+//       include: Airport
+//    });
+
+// console.log(JSON.stringify(city.toJSON(), null, 2));
+// }
+// test();
+
+ Airplane.create({modelNumber:'Bombardier CRJ'});
